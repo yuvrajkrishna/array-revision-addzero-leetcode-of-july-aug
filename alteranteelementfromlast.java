39b2c3488a90f471520243f752fbd47263d73fec
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-08-14 19:31:16 +0530

    alternate element from last

---
+++ alteranteelementfromlast.java
@@ -0,0 +1,8 @@
+public class alteranteelementfromlast {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7,8,9,10};
+        for(int i = arr.length - 1 ; i >= 0 ; i-=2){
+            System.out.println(arr[i]);
+        }
+    }
+}
