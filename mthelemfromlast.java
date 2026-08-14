4e0ac1f661a5bd53344596856358bcd980a151e0
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-08-14 19:28:09 +0530

    mth elem from last

---
+++ mthelemfromlast.java
@@ -0,0 +1,7 @@
+public class mthelemfromlast {
+    public static void main(String[] args) {
+        int arr[] = {2,4,6,8,10};
+        int m = 4;
+        System.out.println(arr[arr.length-m]);
+    }
+}
