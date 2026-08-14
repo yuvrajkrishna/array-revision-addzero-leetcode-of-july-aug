f4a5f5d19d26ee2efc2249f0b015a1c431d265c8
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-08-14 20:26:56 +0530

    double size array

---
+++ DoubleSizeArray.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class DoubleSizeArray {
+    public static void main (String args []){
+        int arr [] = {1,2,3,4,5};
+        int ans [] = new int [arr.length*2];
+        for(int i =0 ; i < arr.length ; i++){
+            ans[i] = arr[i];
+            ans[i+arr.length] = arr[i];
+        }
+        System.out.println(Arrays.toString(ans));
+    }
+}
