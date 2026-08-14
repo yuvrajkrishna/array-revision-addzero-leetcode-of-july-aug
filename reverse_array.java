579054ea5d4b02083c8b834397faa657bf5998e0
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-08-14 19:08:36 +0530

    reverse array

---
+++ reverse_array.java
@@ -0,0 +1,8 @@
+public class reverse_array {
+    public static void main (String args []){
+        int arr [] = {1,2,3,4,5};
+        for(int i = arr.length - 1 ; i >= 0 ; i--){
+            System.out.println(arr[i]);
+        }
+    }
+}
