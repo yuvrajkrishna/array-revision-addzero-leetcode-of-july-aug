46da87abb81f02a6e7ad43942c7c578a19d7f526
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-08-15 20:09:25 +0530

    search in sorted array using binary search

--- SearchInSortedArr.java
+++ SearchInSortedArr.java
@@ -2,19 +2,20 @@ public class SearchInSortedArr {
     public static void main(String[] args) {
         int arr [] = {1,2,3,4,5,6,7,8,9};
         int target = 6 ;
-        int i = 0 ;
-        int j = arr.length - 1; 
-        while (i <=j){
-            if(arr[i] == target){
-                System.out.println(i);
-                break;
+       int start = 0 ;
+       int end = arr.length - 1 ;
+        while (start <= end){
+            int mid = (start + end)/2;
+            if(arr[mid] == target){
+                System.out.println(mid);
+                return;
             }
-            else if (arr[j] == target){
-                System.out.println(j);
-                break;
+            else if (arr[mid] > target){
+                end = mid - 1;
             }
-            i++;
-            j--;
+            else{
+                start = mid + 1 ;
+            }
         }
     }
 }
