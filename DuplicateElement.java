1a059859bec83a388233e870805268a77cfcacfc
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-08-15 09:36:59 +0530

    duplicate eleem 2

--- DuplicateElement.java
+++ DuplicateElement.java
@@ -1,22 +1,28 @@
 public class DuplicateElement {
     public static void main(String[] args) {
-        int arr [] = {1,2,2,3,1,5,4};
+
+        int[] arr = {1, 2, 2, 3, 1, 5, 4};
         int target = 5;
-        int freq = 0;
-        for(int i = 0 ; i < arr.length ; i++){
-            if(arr[i] == target){
-                freq++;
-                if(freq > 1){
+
+        boolean found = false;
+
+        for (int num : arr) {
+
+            if (num == target) {
+
+                if (found) {
                     System.out.println("Duplicate Element");
                     return;
                 }
+
+                found = true;
             }
         }
-        if(freq == 0){
-            System.out.println("Target doesn't Exist");
-        }
-        else{
-            System.out.println("UniqueElement");
+
+        if (found) {
+            System.out.println("Unique Element");
+        } else {
+            System.out.println("Target Doesn't Exist");
         }
     }
 }
