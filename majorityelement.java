6922a21f13d0116736e772791b97d773341966e8
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-08-24 20:47:54 +0530

    majority element

--- majorityelement.java
+++ majorityelement.java
@@ -1,24 +1,21 @@
 public class majorityelement{
-    public static void main(String[] args) {
-        int arr [] ={3,2,3};
-        int n = arr.length-1;
-        int i = 0 ;
-        int j = i+1;
-        int count = 1;
-        while(i < arr.length -1 && j < arr.length){
-            if(arr[i] == arr[j]){
+    public static void main(String args []) {
+        int nums[] = {2,2,1,1,1,2,2};
+        int candidate = 0;
+        int count = 0;
+        for (int num : nums) {
+
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
                 count++;
+            } else {
+                count--;
             }
-            if(count > (n/2)){
-                System.out.println(arr[i]);
-                break;
-            }
-            if(j == arr.length){
-                i++;
-                j=i+1;
-            }
-            j++;
         }
-        System.out.println("not present");
+
+        System.out.println(candidate);
     }
 }
