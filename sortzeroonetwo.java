27267fb99b5db00051b731bf9fe5937bcd47a978
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-08-30 12:51:53 +0530

    sort zero one two

---
+++ sortzeroonetwo.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class sortzeroonetwo{
+    public static void main(String[] args) {
+        int arr[] = {1, 0, 2, 1, 0};
+        int zero = 0;
+        for(int i = 0 ; i < arr.length; i++){
+            if(arr[i] == 0){
+                zero++;
+            }
+        }
+        int one = 0;
+        for(int i = 0 ; i < arr.length ; i++){
+            if(arr[i]==1){
+                one++;
+            }
+        }
+        int two = 0;
+        for(int i = 0 ; i < arr.length ; i++){
+            if(arr[i]==2){
+                two++;
+            }
+        }
+        for(int i = 0 ; i < zero ; i++){
+            arr[i] = 0;
+        }
+        for(int i = zero ; i < zero+one; i++){
+            arr[i] = 1;
+        }
+        for(int i = zero+one ; i < zero+one+two; i++){
+            arr[i] = 2;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
